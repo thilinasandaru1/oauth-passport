69a2b4092461acffe73b51ac54ccf35a98d9a45b
Thilina <thilinasandaru1@gmail.com>
2019-05-13 18:38:15 +0530

    added some auth routes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -4,3 +4,17 @@ const router = require("express").Router();
 router.get("/login", (req, res) => {
   res.render("login");
 });
+
+// auth logout
+router.get("/logout", (req, res) => {
+  // handle with passport
+  res.send("logging out");
+});
+
+// auth with google
+router.get("/google", (req, res) => {
+  // handle with passport
+  res.send("loggin with google");
+});
+
+module.exports = router;
