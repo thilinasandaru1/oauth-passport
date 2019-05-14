a708d4933c25e81baf9d809e9df2fc4f0f2e3f51
Thilina <thilinasandaru1@gmail.com>
2019-05-14 22:03:51 +0530

    serialize and deserialize user

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,6 +4,16 @@ const GoogleStrategy = require("passport-google-oauth20");
 const keys = require("./keys");
 const User = require("../models/user-model");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -18,6 +28,7 @@ passport.use(
         if (currentUser) {
           // already have the user
           console.log("user is: ", currentUser);
+          done(null, currentUser);
         } else {
           // if not, create user in our db
           new User({
@@ -28,6 +39,7 @@ passport.use(
             .then(newUser => {
               console.log("new user created: ", newUser);
             });
+          done(null, newUser);
         }
       });
     }
