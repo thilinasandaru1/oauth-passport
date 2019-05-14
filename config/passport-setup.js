6844ba49705c9c471ae45441ee6f844ca7cfc69f
Thilina <thilinasandaru1@gmail.com>
2019-05-14 17:32:48 +0530

    update google strategy with keys.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,12 +4,17 @@ const GoogleStrategy = require("passport-google-oauth20");
 const keys = require("./keys");
 
 passport.use(
-  new GoogleStrategy({
-    // options for the google strategy
-    clientID: keys.google.clientID,
-    clientSecret: keys.google.clientSecret
-  }),
-  () => {
-    // passport callback function
-  }
+  new GoogleStrategy(
+    {
+      // options for the google strategy
+      callbackURL: "/auth/google/redirect",
+      clientID: keys.google.clientID,
+      clientSecret: keys.google.clientSecret
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // passport callback function
+      console.log("passport callback fired");
+      console.log(profile);
+    }
+  )
 );
